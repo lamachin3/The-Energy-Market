1a3dcfe17ef35e1f4f977bc7d115ca249a2e0cbc
reynokart <75025335+reynokart@users.noreply.github.com>
2021-03-27 17:28:39 +0100

    Commentaire de HOME et SHARING_PLATFORM

--- home.py
+++ home.py
@@ -4,8 +4,8 @@ import math
 import sysv_ipc
 import time, os
  
-key_market = 123
-key_share = 321
+key_market = 123 #clé de la message queue du market
+key_share = 321 #clé de la messager queue du partage
 
 class Home(Process):
 
@@ -19,12 +19,12 @@ class Home(Process):
 
     def run(self):
         try:
-            mq_market = sysv_ipc.MessageQueue(key_market)
+            mq_market = sysv_ipc.MessageQueue(key_market) #Création d'une message queue pour communiquer avec le market
         except:
             print("Cannot connect to message queue", key_market, ", terminating.")
             exit(1)
         
-        mq_share = sysv_ipc.MessageQueue(key_share)  
+        mq_share = sysv_ipc.MessageQueue(key_share) #Création d'une message queue pour communiquer avec les autres maisons
 
         t0 = time.time()
         while True:
@@ -33,13 +33,15 @@ class Home(Process):
                 self.getCons()
                 self.getProd()
                 self.buy_or_sell(mq_market, self.share(mq_share))
-                t0 = t1       
+                t0 = t1  
 
+    #calcul de la consommation de la maison
     def getCons(self): 
         #self.cons = round(100 + ((self.temperature.value - 20)/2)**2 + random.randint(-20, 20))
-        self.cons = random.randint(0, 200)
+        self.cons = random.randint(0, 200) 
         #print("Conso : {} - {}".format(self.cons, os.getpid()))
 
+    #calcul de la production de la maison 
     def getProd(self):
         #self.prod = round(8*self.temperature.value + random.randint(-50, 50))
         self.prod = random.randint(0, 100)
@@ -47,24 +49,26 @@ class Home(Process):
             self.prod = 0
         #print("Prod : {} - {}".format(self.prod, os.getpid()))
 
+    #achat et vente sur le marché
     def buy_or_sell(self, mq_market, gap):
         encd_gap = str(abs(gap)).encode()
         if gap > 0:
-            mq_market.send(encd_gap, type = 1)    
+            mq_market.send(encd_gap, type = 1) #demande de vente d'energie sur le marché
         elif gap < 0:
-            mq_market.send(encd_gap, type=2)
+            mq_market.send(encd_gap, type=2) #demande d'achat d'energie sur le marché
 
+    #partage des ressources avec les autres maisons
     def share(self, mq_share):
-        gap = self.prod - self.cons
+        gap = self.prod - self.cons #calcul du reste de l'energie après consommation
         print("gap", gap, "\tpid", os.getpid())
         if self.policy == 2 and gap > 0:
-            msg = "0_" + str(os.getpid())
+            msg = "0_" + str(os.getpid()) # pas de partage
         else:
-            msg = str(gap) + "_" + str(os.getpid())
+            msg = str(gap) + "_" + str(os.getpid()) #création du message pour la message queue avec l'écart et le PID sous la forme (GAP_PID)
         msg = msg.encode()
         
-        mq_share.send(msg, type=self.policy)   
-        msg_rcvd = mq_share.receive(type=os.getpid())[0].decode()
+        mq_share.send(msg, type=self.policy) #envoi du message de type = policy
+        msg_rcvd = mq_share.receive(type=os.getpid())[0].decode() #réception du message de type = PID
         if self.policy == 2 and gap > 0:
-            msg_rcvd = gap
+            msg_rcvd = gap #réatribution de l'écart non mis sur le partage 
         return int(msg_rcvd)
--- sharing_platform.py
+++ sharing_platform.py
@@ -11,54 +11,54 @@ class SharingPlatform(Process):
         super().__init__()
         self.nb_houses = nb_houses
         try:
-            self.mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
+            self.mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX) #crée une message queue 
         except:
-            print("Message queue", key, "already exsits, terminating.")
+            print("Message queue", key, "already exists, terminating.")
             sys.exit(1)
 
     def run(self):
         while True:
-            if self.mq.current_messages == self.nb_houses:
+            if self.mq.current_messages == self.nb_houses: #attend que toutes les maisons aient envoyé un message
                 data_rcvd = dict()
-                while self.mq.current_messages != 0:
-                    msg = self.mq.receive(type=-3)[0].decode()
-                    msg_split = msg.split("_")
-                    data_rcvd[msg_split[1]] = int(msg_split[0])
+                while self.mq.current_messages != 0: #tant qu'il y a encore des messages à traiter
+                    msg = self.mq.receive(type=-3)[0].decode() #traite tous les messages de type <= 3 en commençant par le type le plus petit (priorisation des échanges pour les policy 1 (energie perdu sinon))
+                    msg_split = msg.split("_") #sépare le GAP du PID 
+                    data_rcvd[msg_split[1]] = int(msg_split[0]) #enregistre les informations dans un dictionnaire
 
-                data_send = self.processing(data_rcvd)
+                data_send = self.processing(data_rcvd) #effectue les échanges entre les maisons puis renvoie l'energie restante après transaction dans un autre dictionnaire
                 for i in data_send:
-                    self.mq.send(str(data_send[i]).encode(), type=int(i))
+                    self.mq.send(str(data_send[i]).encode(), type=int(i)) #envoie la nouvelle valeur après échange à leur propriétaire à l'aide du PID
 
     def processing(self, rcvd_dict):
         needed_dict = dict()
         available_dict = dict()
         for i in rcvd_dict:
-            if rcvd_dict[i] < 0:
-                needed_dict[i] = abs(rcvd_dict[i])
-            elif rcvd_dict[i] > 0:
-                available_dict[i] = rcvd_dict[i]
+            if rcvd_dict[i] < 0: #besoin d'energie
+                needed_dict[i] = abs(rcvd_dict[i]) #regroupe toutes les maisons qui ont besoin d'energie dans le dictionnaire needed_dict
+            elif rcvd_dict[i] > 0: #surplus d'energie
+                available_dict[i] = rcvd_dict[i] #regroupe toutes les maisons qui ont un surplus d'energie dans le dictionnaire needed_dict
 
-        while needed_dict and available_dict:
-            key1, key2 = list(needed_dict.keys())[0], list(available_dict.keys())[0]
-            val_need, val_avlb = needed_dict[key1], available_dict[key2]
-            if val_need == val_avlb:
-                needed_dict.pop(key1)
-                available_dict.pop(key2)
-            elif val_need < val_avlb:
-                needed_dict.pop(key1)
-                available_dict[key2] = val_avlb - val_need
-            else:
-                available_dict.pop(key2)
-                needed_dict[key1] = val_need - val_avlb
+        while needed_dict and available_dict: #tant qu'il y a des besoins ET des surplus d'energie
+            key1, key2 = list(needed_dict.keys())[0], list(available_dict.keys())[0] #récupere la clé du premier élément de chaque dictionnaire
+            val_need, val_avlb = needed_dict[key1], available_dict[key2] #récupere la valeur associée à cette clé
+            if val_need == val_avlb: #échange parfait tous les deux n'ont ni surplus ni besoin
+                needed_dict.pop(key1) #la maison de needed n'a plus de besoin
+                available_dict.pop(key2) #la maison de available n'a plus d'energie 
+            elif val_need < val_avlb: #comble le besoin mais il reste un surplus
+                needed_dict.pop(key1) #la maison de needed n'a plus de besoin
+                available_dict[key2] = val_avlb - val_need #calcul le surplus restant pour la maison de available
+            else: #ne comble pas le besoin mais le réduit, la maison dans available donne tout ce qu'elle a 
+                available_dict.pop(key2) #la maison de available n'a plus d'energie 
+                needed_dict[key1] = val_need - val_avlb #calcul le besoin restant pour la maison de needed
         
         for i in rcvd_dict:
-            rcvd_dict[i] = 0
+            rcvd_dict[i] = 0 #considérons que toutes les transactions ont conduit à un parfait échange
         
-        if needed_dict:
+        if needed_dict: #il reste des besoins d'energie après échange
             for i in needed_dict:
-                rcvd_dict[i] = -needed_dict[i]
-        elif available_dict:
+                rcvd_dict[i] = -needed_dict[i] #rajoute les besoins restants après la transaction dans rcvd
+        elif available_dict: #il reste des surplus d'energie après échange
             for i in available_dict:
-                rcvd_dict[i] = available_dict[i]
-
+                rcvd_dict[i] = available_dict[i] #rajoute les surplus d'energie restants après la transaction dans rcvd
+        #else : echange parfait donc aucune action puisqu'il confirme l'hypothèse ligne 55
         return rcvd_dict
